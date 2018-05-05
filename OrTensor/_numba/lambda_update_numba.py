2b7ebc0c410750e4538bcb8aebf1382642563647
Shiloh SQ. Leung <zszx0259@me.com>
2018-05-05 23:31:55 +0800

    finish accumulator caculation

---
+++ OrTensor/_numba/lambda_update_numba.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/5 下午10:27
+# @Author  : Shiloh Leung
+# @Site    : 
+# @File    : lambda_update_numba.py
+# @Software: PyCharm
---
+++ OrTensor/auxiliary_lib.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/5 下午3:29
+# @Author  : Shiloh Leung
+# @Site    : 
+# @File    : auxiliary_lib.py
+# @Software: PyCharm
