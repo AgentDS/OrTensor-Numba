84cbcd6fca5820ebd9a3d885771f5eba106678d6
Shiloh SQ. Leung <zszx0259@me.com>
2018-05-06 14:08:01 +0800

    replace prange with range for faster speed

--- OrTensor/_numba/basic.py
+++ OrTensor/_numba/basic.py
@@ -6,6 +6,7 @@
 # @File    : basic.py
 # @Software: PyCharm
 import numpy as np
+import numba
 from numba import jit, prange
 
 """
@@ -24,27 +25,28 @@ def Triple_Boolean_Inner_product(A_i, B_j, C_k):
     :return: 2*min(1, sum(A_i.*B_j.*C_k)) - 1
     """
     R = A_i.shape[0]
-    for r in prange(R):
-        if A_i[r]*B_j[r]*C_k[r] == 1:
-            return 1
-    return -1
-
-
-@jit('int8(int8[:], int8[:], int8[:])', nopython=True, nogil=True)
-def Triple_Boolean_Inner_product2(A_i, B_j, C_k):
-    """
-    Calculate the Boolean product of three vectors of the same length.
-
-    :param A_i: R, the i_{th} row in factor matrix A
-    :param B_j: R, the j_{th} row in factor matrix B
-    :param C_k: R, the k_{th} row in factor matrix C
-    :return: 2*min(1, sum(A_i.*B_j.*C_k)) - 1
-    """
-    R = A_i.shape[0]
+    # if R <=10,000,000, use range(), else use prange()
     for r in range(R):
         if A_i[r]*B_j[r]*C_k[r] == 1:
             return 1
     return -1
+
+
+# @jit('int8(int8[:], int8[:], int8[:])', nopython=True, nogil=True)
+# def Triple_Boolean_Inner_product2(A_i, B_j, C_k):
+#     """
+#     Calculate the Boolean product of three vectors of the same length.
+#
+#     :param A_i: R, the i_{th} row in factor matrix A
+#     :param B_j: R, the j_{th} row in factor matrix B
+#     :param C_k: R, the k_{th} row in factor matrix C
+#     :return: 2*min(1, sum(A_i.*B_j.*C_k)) - 1
+#     """
+#     R = A_i.shape[0]
+#     for r in range(R):
+#         if A_i[r]*B_j[r]*C_k[r] == 1:
+#             return 1
+#     return -1
 
 
 # @jit()
--- tests/time_test.py
+++ tests/time_test.py
@@ -5,13 +5,14 @@
 # @Site    : 
 # @File    : time_test.py
 # @Software: PyCharm
-from OrTensor._numba.basic import Triple_Boolean_Inner_product as inner
-from OrTensor._numba.basic import Triple_Boolean_Inner_product2 as inner2
+from OrTensor._numba.basic import Triple_Boolean_Inner_product as inner1
+#from OrTensor._numba.basic import Triple_Boolean_Inner_product2 as inner2
 import time
 import numpy as np
 
+
 def test_inner_prod_time():
-    R = 1000
+    R = 10000000
     a = np.zeros(R, dtype=np.int8)
     b = np.zeros(R, dtype=np.int8)
     c = np.zeros(R, dtype=np.int8)
@@ -20,17 +21,18 @@ def test_inner_prod_time():
     c[R - 2] = 1
     print("Using numba.prange():  ", end='')
     start1 = time.time()
-    res1 = inner(a, b, c)
+    for i in range(1000):
+        res1 = inner1(a, b, c)
     end1 = time.time()
     print("ans = %d, time = %.8fs" % (res1, end1 - start1))
 
-    print("Using range():  ", end='')
-    start2 = time.time()
-    res2 = inner2(a, b, c)
-    end2 = time.time()
-    print("ans = %d, time = %.8fs" % (res2, end2 - start2))
+    # print("Using range():  ", end='')
+    # start2 = time.time()
+    # for i in range(1000):
+    #     res2 = inner2(a, b, c)
+    # end2 = time.time()
+    # print("ans = %d, time = %.8fs" % (res2, end2 - start2))
 
 
-
-if __name__=='__main__':
-    test_inner_prod_time()
+if __name__ == '__main__':
+    # test_inner_prod_time()
